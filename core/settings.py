c2da5a22387a64cd830d3225af138bd0677540cc
dev-chirag-taneja <chiragtaneja915@gmail.com>
2023-07-27 15:34:46 +0530

    vercel settings

--- core/settings.py
+++ core/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = BASE_DIR / 'static'
-# STATIC_ROOT = BASE_DIR / 'staticfiles_build' /'static'
+STATICFILES_DIRS = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 # Media files
 MEDIA_URL = 'image/'
